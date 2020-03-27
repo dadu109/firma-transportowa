397485ded2eeba8dc93afe38908f2b48b30bd597
dadu109 <heniek12322@gmail.com>
2020-03-28 00:56:09 +0100

    hero section

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const burger = document.querySelector('.burger');
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const swiperContainer = document.querySelector('.swiper-container');
+const navWrapper = document.querySelector('.nav-wrapper');
 
 let burgerOpen = false;
 
-burger.addEventListener('click',()=>{
-   if(burgerOpen){
-       burger.classList.remove('burger--open');
-       mobileMenu.classList.remove('mobile-menu--open')
-       burgerOpen=false;
-   }else{
-       burger.classList.add('burger--open');
-       mobileMenu.classList.add('mobile-menu--open');
-       burgerOpen=true;
-   }
+const mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    simulateTouch: true,
+    effect:'fade',
+    fadeEffect: {
+        crossFade: true
+    },
 });
 
+const adjustSwiperHeight = (e) => {
+    if (window.innerHeight < 1000) {
+        swiperContainer.style.height = `${window.innerHeight - navWrapper.clientHeight}px`
+    }else{
+        swiperContainer.style.height = '1000px';
+    }
+};
+
+burger.addEventListener('click', () => {
+    if (burgerOpen) {
+        burger.classList.remove('burger--open');
+        mobileMenu.classList.remove('mobile-menu--open');
+        burgerOpen = false;
+    } else {
+        burger.classList.add('burger--open');
+        mobileMenu.classList.add('mobile-menu--open');
+        burgerOpen = true;
+    }
+});
+
+window.addEventListener('resize', adjustSwiperHeight);
+
+
+adjustSwiperHeight();
